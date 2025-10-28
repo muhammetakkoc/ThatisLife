8abc9b3dd6b72bba3a241c98125cc1e6f399fa56
muhammetakkoc <akkoc.muhammet0@gmail.com>
2025-10-28 16:00:42 -0400

    Wood Chop Done

--- Assets/Scripts/ChopWood.cs
+++ Assets/Scripts/ChopWood.cs
@@ -3,21 +3,92 @@ using UnityEngine.UI;
 
 public class ChopWood : MonoBehaviour
 {
-    public Text E_intteract;
-    
+    public KeyCode chopKey = KeyCode.E;
+    public float holdTime = 1.2f;
+    public Slider chopSlider;
+
+    private GameObject interactText;
+    private bool playerInRange = false;
+    private float timer = 0f;
+
+    private static ChopWood active = null;   
+    private static int choppedTrees = 0;     
+
     void Start()
     {
-        
+        interactText = GameObject.Find("InteractText");
+        if (interactText) interactText.SetActive(false);
+
+        if (chopSlider)
+        {
+            chopSlider.gameObject.SetActive(false);
+            chopSlider.minValue = 0; chopSlider.maxValue = 1; chopSlider.value = 0;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!playerInRange) return;
+
         
+        if (active != null && active != this) return;
+
+        
+        if (Input.GetKeyDown(chopKey) && active == null)
+            active = this;
+
+      
+        if (active == this && Input.GetKey(chopKey))
+        {
+            timer += Time.deltaTime;
+            if (chopSlider)
+            {
+                chopSlider.gameObject.SetActive(true);
+                chopSlider.value = timer / holdTime;
+            }
+            if (timer >= holdTime)
+                ChopTree();
+        }
+
+      
+        if (active == this && Input.GetKeyUp(chopKey))
+            ResetProgress(releaseActive: true);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void ChopTree()
     {
-        
+        choppedTrees++;
+        Debug.Log("Tree Chopped: " + choppedTrees);
+
+        if (interactText) interactText.SetActive(false);
+        if (chopSlider) chopSlider.gameObject.SetActive(false);
+
+        active = null; 
+        Destroy(gameObject);
+    }
+
+    void ResetProgress(bool releaseActive)
+    {
+        timer = 0f;
+        if (chopSlider) chopSlider.value = 0f;
+        if (releaseActive && active == this) active = null;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        playerInRange = true;
+        if (interactText) interactText.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        playerInRange = false;
+        if (interactText) interactText.SetActive(false);
+        if (chopSlider) { chopSlider.gameObject.SetActive(false); chopSlider.value = 0; }
+        ResetProgress(releaseActive: true);
     }
 }
+
+
