6a69f6acc44e087060913df396bd947e4f5d6a60
muhammetakkoc <akkoc.muhammet0@gmail.com>
2025-12-04 05:31:48 -0500

    Fishing done

--- Assets/Scripts/InventoryUIBinder.cs
+++ Assets/Scripts/InventoryUIBinder.cs
@@ -3,15 +3,22 @@ using UnityEngine.UI;
 
 public class InventoryUIBinder : MonoBehaviour
 {
+    public GameObject panel;
     public Text woodText;
     public Text fishText;
     public Text cropText;
     public Text coinText;
 
-    void Awake()
+    void Start()
     {
         if (InventoryManager.I != null)
-            InventoryManager.I.BindUI(gameObject, woodText, fishText, cropText, coinText);
+        {
+            InventoryManager.I.BindUI(panel, woodText, fishText, cropText, coinText);
+        }
+        else
+        {
+            Debug.LogWarning("InventoryManager yok! ?lk sahnede olu?tu?undan emin ol.");
+        }
     }
 }
 
